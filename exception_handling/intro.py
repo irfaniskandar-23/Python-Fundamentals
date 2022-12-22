5de7bbd5d496d310e074064844069ccaf8da1014
irfaniskandar-23 <irfaniskandar791@gmail.com>
2022-12-22 20:59:06 +0800

    second commit

---
+++ exception_handling/intro.py
@@ -0,0 +1,14 @@
+'''
+Exception handling in python
+
+
+try:
+    statement
+except:
+    catch Exception BaseException
+else:
+    statement if no exception raised
+
+finally:
+     statement that executes regardless of try/except block result
+'''
